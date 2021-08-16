bd4ef3a677ec1e0c4ff2a528f50df59749aa7c79
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-08-15 20:22:45 -0400

    typescript template with modifications

---
+++ navigation/BottomTabNavigator.tsx
@@ -0,0 +1,77 @@
+/**
+ * Learn more about createBottomTabNavigator:
+ * https://reactnavigation.org/docs/bottom-tab-navigator
+ */
+
+import { Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as React from 'react';
+
+import Colors from '../constants/Colors';
+import useColorScheme from '../hooks/useColorScheme';
+import HomeScreen from '../screens/HomeScreen';
+import NotifsScreen from '../screens/NotifsScreen';
+import { BottomTabParamList, HomeParamList, NotifsParamList } from '../types';
+
+const BottomTab = createBottomTabNavigator<BottomTabParamList>();
+
+export default function BottomTabNavigator() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <BottomTab.Navigator
+      initialRouteName="Home"
+      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}>
+      <BottomTab.Screen
+        name="Home"
+        component={HomeNavigator}
+        options={{
+          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Notifs"
+        component={NotifsNavigator}
+        options={{
+          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+}
+
+// You can explore the built-in icon families and icons on the web at:
+// https://icons.expo.fyi/
+function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string }) {
+  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
+}
+
+// Each tab has its own navigation stack, you can read more about this pattern here:
+// https://reactnavigation.org/docs/tab-based-navigation#a-stack-navigator-for-each-tab
+const HomeStack = createStackNavigator<HomeParamList>();
+
+function HomeNavigator() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+      />
+    </HomeStack.Navigator>
+  );
+}
+
+const NotifsStack = createStackNavigator<NotifsParamList>();
+
+function NotifsNavigator() {
+  return (
+    <NotifsStack.Navigator>
+      <NotifsStack.Screen
+        name="NotifsScreen"
+        component={NotifsScreen}
+        options={{ headerTitle: 'Notifications' }}
+      />
+    </NotifsStack.Navigator>
+  );
+}
---
+++ types.tsx
@@ -0,0 +1,22 @@
+/**
+ * Learn more about using TypeScript with React Navigation:
+ * https://reactnavigation.org/docs/typescript/
+ */
+
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  Home: undefined;
+  Notifs: undefined;
+};
+
+export type HomeParamList = {
+  HomeScreen: undefined;
+};
+
+export type NotifsParamList = {
+  NotifsScreen: undefined;
+};
