88bac129df39a3477767530d28331310dfa80c4f
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-08-15 20:45:10 -0400

    calendar and settings screens

--- screens/CalendarScreen.tsx
+++ screens/CalendarScreen.tsx
@@ -4,12 +4,12 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
-export default function HomeScreen() {
+export default function CalendarScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
+      <Text style={styles.title}>Calendar</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/HomeScreen.tsx" />
+      <EditScreenInfo path="/screens/CalendarScreen.tsx" />
     </View>
   );
 }
--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -4,12 +4,12 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
-export default function HomeScreen() {
+export default function SettingsScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
+      <Text style={styles.title}>Settings</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/HomeScreen.tsx" />
+      <EditScreenInfo path="/screens/SettingsScreen.tsx" />
     </View>
   );
 }
