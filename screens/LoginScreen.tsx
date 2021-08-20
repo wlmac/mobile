b5c7094140b5875607725c8f22441e0d812cfafb
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-08-20 19:02:25 -0400

    login screen!

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
 import { StyleSheet, TextInput, Button } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import config from '../config.json';
 import { Text, View } from '../components/Themed';
+
+let state = {
+  username: "",
+  password: ""
+}
 
 export default function LoginScreen() {
   return (
@@ -9,11 +16,16 @@ export default function LoginScreen() {
       <Text style={styles.title}>Login</Text>
       <View>
         <TextInput
-          placeholder="Email" />
+          style={styles.textInput}
+          placeholder="Username"
+          onChangeText={(value) => state.username = value} />
         <TextInput
+          style={styles.textInput}
           secureTextEntry={true}
           placeholder="Password"
+          onChangeText={(value) => state.password = value}
         />
+        <Button onPress={login} title="Login"></Button>
       </View>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
     </View>
@@ -35,4 +47,32 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  textInput: {
+    color: 'white'
+  }
 });
+
+function login() {
+  fetch(`${config.server}/api/auth/token`, { //refresh token endpoint
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: state.username,
+      password: state.password
+    })
+  }).then((response) => response.json())
+    .then((json) => {
+      if (json.access && json.refresh) {
+        AsyncStorage.setItem('@accesstoken', json.access).then(() => {
+          AsyncStorage.setItem('@refreshtoken', json.refresh).then(() => {
+            //redirect to home page ig
+          }).catch(err => console.log(err));
+        }).catch(err => console.log(err));
+      }
+      else {
+        console.log('Not nice :((');
+      }
+    }).catch(err => console.log(err));
+}
