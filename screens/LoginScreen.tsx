75f42b06a8992b576dfcab04353d2d2f221b45fc
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-08-20 23:49:31 -0400

    temp commit

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,26 +1,32 @@
 import * as React from 'react';
-import { StyleSheet, TextInput, Button } from 'react-native';
+import { StyleSheet, TextInput, Button, ColorSchemeName } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import config from '../config.json';
 import { Text, View } from '../components/Themed';
+import Navigation from '../navigation';
+import App from '../App';
 
 let state = {
   username: "",
-  password: ""
+  password: "",
+  colorScheme: ""
 }
 
-export default function LoginScreen() {
+export default function LoginScreen(colorScheme: string) {
+  state.colorScheme = colorScheme;
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Login</Text>
       <View>
         <TextInput
-          style={styles.textInput}
+          style={{ color: (colorScheme == "dark") ? "white" : "black" }}
           placeholder="Username"
           onChangeText={(value) => state.username = value} />
         <TextInput
-          style={styles.textInput}
+          style={{ color: (colorScheme == "dark") ? "white" : "black" }}
           secureTextEntry={true}
           placeholder="Password"
           onChangeText={(value) => state.password = value}
@@ -46,9 +52,6 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
-  },
-  textInput: {
-    color: 'white'
   }
 });
 
@@ -68,10 +71,13 @@ function login() {
         AsyncStorage.setItem('@accesstoken', json.access).then(() => {
           AsyncStorage.setItem('@refreshtoken', json.refresh).then(() => {
             //redirect to home page ig
+            console.log('very nice!');
+            //App();
           }).catch(err => console.log(err));
         }).catch(err => console.log(err));
       }
       else {
+        //edit text to add "Wrong username or password" here
         console.log('Not nice :((');
       }
     }).catch(err => console.log(err));
