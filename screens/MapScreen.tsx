5cd7dead03f64c49a610a50e67e89ef8b8ffafd1
aava <aavachhori@gmail.com>
2021-08-24 10:33:23 -0400

    focous map onto wlmac, enable user location

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,17 +1,65 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { Text, View } from '../components/Themed';
+import * as Permission from 'expo-permissions'
+
+import * as Location from 'expo-location';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useRef } from 'react';
 
 export default function MapScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Map</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/MapScreen.tsx" />
-    </View>
-  );
+  const LATITUDE_DELTA = 0.00422; 
+    const LONGITUDE_DELTA = 0.00091;
+    const latitude = 43.75376776088882;
+    const longitude = -79.46106695372214;
+    // const mapRef = useRef(); 
+
+    useEffect(() => {
+      (async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          // setErrorMsg('Permission to access location was denied');
+          return;
+        }
+
+      })();
+    }, []);
+    
+
+    return (
+      <View style={styles.container}>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: latitude, 
+              longitude: longitude,
+              latitudeDelta:LATITUDE_DELTA, 
+              longitudeDelta:LONGITUDE_DELTA
+            }}
+            provider = {PROVIDER_GOOGLE}
+            mapType="standard"
+            zoomEnabled={true}
+            pitchEnabled={true}
+            showsUserLocation={true}
+            followsUserLocation={true}
+            showsCompass={true}
+            showsBuildings={true}
+            showsTraffic={true}
+            showsIndoors={true}>
+          </MapView>
+      </View>
+    )
+ 
+    
+    // <View style={styles.container}>
+    //   <Text style={styles.title}>Map</Text>
+    //   <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    //   <EditScreenInfo path="/screens/MapScreen.tsx" />
+    // </View>
+  
 }
 
 const styles = StyleSheet.create({
@@ -29,4 +77,9 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  map: {
+    flex: 1,
+    height: "100%",
+    width: '100%',
+  }
 });
