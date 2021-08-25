4bf13657591f4d2431d3c4423213dc46986e8d1c
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-08-25 17:03:29 -0400

    transitions to root stack

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import { StyleSheet, TextInput, Button, Linking, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RouteProp } from '@react-navigation/native';
 
 import config from '../config.json';
 import { Text, View } from '../components/Themed';
 import Colors from '../constants/Colors';
+import useColorScheme from '../hooks/useColorScheme';
+import { RootStackParamList } from '../types';
 
 let state = {
   username: "",
@@ -12,8 +16,19 @@ let state = {
   colorScheme: ""
 }
 
-export default function LoginScreen(colorScheme: string) {
-  state.colorScheme = colorScheme;
+export default function LoginScreen({ route, navigation }: { route: RouteProp<RootStackParamList, 'Login'>, navigation: StackNavigationProp<RootStackParamList, 'Login'> }) {
+  const colorScheme = useColorScheme();
+  const { loginNeeded } = route.params;
+  let loginPress = () => {
+    login().then(val => {
+      if (val) {
+        navigation.replace('Root');
+      }
+    }).catch(err => console.log(err));
+  }
+  if (!loginNeeded) {
+    navigation.replace('Root');
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Login</Text>
@@ -28,10 +43,10 @@ export default function LoginScreen(colorScheme: string) {
           placeholder="Password"
           onChangeText={(value) => state.password = value}
         />
-        <Button onPress={login} title="Login"></Button>
+        <Button onPress={loginPress} title="Login"></Button>
       </View>
       <View style={styles.helpContainer}>
-        <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
+        <TouchableOpacity onPress={handleRegisterPress} style={styles.helpLink}>
           <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
             Register Here
           </Text>
@@ -70,34 +85,47 @@ const styles = StyleSheet.create({
   },
 });
 
-function handleHelpPress() {
+function handleRegisterPress() {
   Linking.openURL(`${config.server}/accounts/signup/?next=/`).catch(err => console.error("Couldn't load page", err));
 }
 
 function login() {
-  fetch(`${config.server}/api/auth/token`, { //refresh token endpoint
-    method: "POST",
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      username: state.username,
-      password: state.password
-    })
-  }).then((response) => response.json())
-    .then((json) => {
-      if (json.access && json.refresh) {
-        AsyncStorage.setItem('@accesstoken', json.access).then(() => {
-          AsyncStorage.setItem('@refreshtoken', json.refresh).then(() => {
-            //redirect to home page ig
-            console.log('very nice!');
-            //App();
-          }).catch(err => console.log(err));
-        }).catch(err => console.log(err));
-      }
-      else {
-        //edit text to add "Wrong username or password" here
-        console.log('Not nice :((');
-      }
-    }).catch(err => console.log(err));
+  return new Promise((resolve, reject) => {
+    fetch(`${config.server}/api/auth/token`, { //refresh token endpoint
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: state.username,
+        password: state.password
+      })
+    }).then((response) => response.json())
+      .then((json) => {
+        if (json.access && json.refresh) {
+          AsyncStorage.setItem('@accesstoken', json.access).then(() => {
+            AsyncStorage.setItem('@refreshtoken', json.refresh).then(() => {
+              //redirect to home page ig
+              console.log('very nice!');
+              resolve(true);
+              //App();
+            }).catch(err => {
+              console.log(err);
+              resolve(false);
+            });
+          }).catch(err => {
+            console.log(err);
+            resolve(false);
+          });
+        }
+        else {
+          //edit text to add "Wrong username or password" here
+          console.log('Not nice :((');
+          resolve(false);
+        }
+      }).catch(err => {
+        console.log(err);
+        resolve(false);
+      });
+  })
 }
