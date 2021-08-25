8e4ebde1e5c8f70811e9502b6bd396e7254f0ff7
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-08-25 15:54:39 -0400

    pass loginneeded into nav

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -9,16 +9,17 @@ import * as React from 'react';
 import { ColorSchemeName } from 'react-native';
 
 import NotFoundScreen from '../screens/NotFoundScreen';
+import LoginScreen from '../screens/LoginScreen';
 import { RootStackParamList } from '../types';
 import BottomTabNavigator from './BottomTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
 
-export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
+export default function Navigation({ colorScheme, loginNeeded }: { colorScheme: ColorSchemeName, loginNeeded: boolean }) {
   return (
     <NavigationContainer
       linking={LinkingConfiguration}
       theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <RootNavigator />
+      <RootNavigator loginNeeded={loginNeeded} />
     </NavigationContainer>
   );
 }
@@ -27,11 +28,13 @@ export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeNa
 // Read more here: https://reactnavigation.org/docs/modal
 const Stack = createStackNavigator<RootStackParamList>();
 
-function RootNavigator() {
+function RootNavigator({ loginNeeded }: { loginNeeded: boolean }) {
+  //use login as modal instead
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Root" component={BottomTabNavigator} />
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
+      <Stack.Screen name="Login" component={LoginScreen} />
     </Stack.Navigator>
   );
 }
