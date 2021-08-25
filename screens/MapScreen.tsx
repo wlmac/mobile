2715d25e244a32f2f4c338bdd8292535d87276ec
aava <aavachhori@gmail.com>
2021-08-25 12:02:00 -0400

    switch map overlay image depending on altitude

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 
 import MapView, { Overlay, PROVIDER_GOOGLE } from 'react-native-maps';
@@ -6,29 +7,42 @@ import { Text, View } from '../components/Themed';
 import * as Permission from 'expo-permissions'
 
 import * as Location from 'expo-location';
-import { useEffect } from 'react';
+import Constants from 'expo-constants';
+import { Platform } from 'react-native';
+
+
+import EditScreenInfo from '../components/EditScreenInfo';
 
 export default function MapScreen() {
   const LATITUDE_DELTA = 0.00422; 
     const LONGITUDE_DELTA = 0.00091;
     const latitude = 43.75376776088882;
     const longitude = -79.46106695372214;
+    
     // const mapRef = useRef(); 
+    const [location, setAlti] = useState<{altitude: null | number, }>({ altitude: null });
+    var level = 1; 
+    var altitude: number | null = null; 
+    const [errorMsg, setErrorMsg] = useState("");
+
 
     useEffect(() => {
       (async () => {
         let { status } = await Location.requestForegroundPermissionsAsync();
         if (status !== 'granted') {
-          // setErrorMsg('Permission to access location was denied');
+          setErrorMsg('Permission to access location was denied');
           return;
+        }else{
+          let location = await Location.getCurrentPositionAsync();
+          altitude = location.coords.altitude;
+          setAlti(prevState => ({ ...prevState, altitude }))
         }
-
       })();
     }, []);
-    
 
     return (
       <View style={styles.container}>
+          {/* <Text style={styles.title}>{location.altitude}</Text> */}
           <MapView
             style={styles.map}
             initialRegion={{
@@ -48,14 +62,7 @@ export default function MapScreen() {
             showsTraffic={true}
             showsIndoors={true}
             >
-            <Overlay 
-              image={require('../assets/images/Level1WLMACMAP.png')}
-              style={styles.schoolMap}
-              bounds={[
-                [43.75279562211260, -79.46283144388977],
-                [43.7540913854649, -79.46043910295570]
-              ]}
-            /> 
+            {mapOverlay(location.altitude)}
           </MapView>
       </View>
     )
@@ -63,9 +70,6 @@ export default function MapScreen() {
     //Basic positioning
     // [43.75285762211266, -79.46278144388977],
     //               [43.7540213854642, -79.46070519295975]
-
-
- 
     
     // <View style={styles.container}>
     //   <Text style={styles.title}>Map</Text>
@@ -75,6 +79,28 @@ export default function MapScreen() {
   
 }
 
+function mapOverlay( altitude: any) {
+  console.log(altitude);
+  if((altitude>147)){
+    return (<Overlay 
+      image={require('../assets/images/Level2WLMACMAP.png')}
+      bounds={[
+        [43.75279562211260, -79.46283144388977],
+        [43.7540913854649, -79.46043910295570]
+      ]}
+    /> )
+  }else if(altitude<147){
+    return (<Overlay 
+      image={require('../assets/images/Level1WLMACMAP.png')}
+      bounds={[
+        [43.75279562211260, -79.46283144388977],
+        [43.7540913854649, -79.46043910295570]
+      ]}
+    /> )
+  }
+}
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -82,6 +108,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   title: {
+    flex: 1,
     fontSize: 20,
     fontWeight: 'bold',
   },
@@ -95,7 +122,7 @@ const styles = StyleSheet.create({
     height: "100%",
     width: '100%',
   }, 
-  schoolMap: {
-    transform: [{ rotate: '50deg'}],
-  }
 });
+
+
+
