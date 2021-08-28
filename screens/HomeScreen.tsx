1a4c984614d977410418dd606d7707ea501f661b
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-08-28 18:20:50 -0400

    weather info

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,15 +1,32 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import config from '../config.json';
 
 export default function HomeScreen() {
+  let [timetable, updateTimeTable] = React.useState("Fetching data...");
+  let [weatherIcon, updateIcon] = React.useState(require('../assets/images/weather/loading.gif'));
+  let [temp, updateTemp] = React.useState('Loading...');
+  let fetchData = () => {
+    getTimetableInfo().then(data => {
+      //process timetable data
+    })
+  }
+  getWeather().then((data) => {
+    updateIcon(wIcons[data.weather_state_abbr]);
+    updateTemp(`${data.the_temp}°C`);
+  }).catch(() => {
+    updateTemp(`Couldn't fetch :(`);
+  })
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Home</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/HomeScreen.tsx" />
+      <Text>{temp}</Text>
+      <Image style={styles.logo} source={weatherIcon} />
     </View>
   );
 }
@@ -29,4 +46,64 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  logo: {
+    width: 66,
+    height: 58,
+  },
 });
+
+function getTimetableInfo() {
+  return new Promise((resolve, reject) => {
+    AsyncStorage.getItem('@accesstoken').then(token => {
+      fetch(`${config.server}/api/timetables`, { //refresh token endpoint
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          access: token
+        })
+      }).then((response) => response.json())
+        .then((json) => {
+          //resolve data after processing (waiting on sample data)
+        }).catch(err => {
+          console.log(err);
+          resolve("Network error. Please try again later.");
+        });
+    }).catch(err => resolve("Error reading user data"));
+  })
+}
+
+function getWeather() {
+  return new Promise<Weather>((resolve, reject) => {
+    fetch(`${config.weatherserver}/api/location/4118/`).then((response) => response.json()).then((json) => {
+      json.consolidated_weather[0];
+      let weather = new Weather(json.consolidated_weather[0].weather_state_abbr, json.consolidated_weather[0].the_temp);
+      resolve(weather);
+    }).catch(err => reject("network"));
+  })
+}
+
+class Weather {
+  weather_state_abbr: string;
+  the_temp: number;
+  constructor(weather_state_abbr: string, the_temp: string) {
+    this.weather_state_abbr = weather_state_abbr;
+    this.the_temp = Math.round(parseFloat(the_temp));
+  }
+}
+
+const wIcons: {
+  [key: string]: any
+} = {
+  c: require(`../assets/images/weather/c.png`),
+  h: require(`../assets/images/weather/h.png`),
+  hc: require(`../assets/images/weather/hc.png`),
+  hr: require(`../assets/images/weather/hr.png`),
+  lc: require(`../assets/images/weather/lc.png`),
+  lr: require(`../assets/images/weather/lr.png`),
+  s: require(`../assets/images/weather/s.png`),
+  sl: require(`../assets/images/weather/sl.png`),
+  sn: require(`../assets/images/weather/sn.png`),
+  t: require(`../assets/images/weather/t.png`)
+}
