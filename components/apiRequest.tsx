c9d99dd633889cc0316ba26fb6e853b737a87893
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-08-28 23:50:40 -0400

    wrap api requests

---
+++ components/apiRequest.tsx
@@ -0,0 +1,47 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Alert } from 'react-native';
+import RNExitApp from 'react-native-exit-app';
+
+import config from '../config.json';
+import defaultLogin from './defaultLogin';
+
+export default function apiRequest(endpoint: string, body: string): Promise<ApiResponse> {
+    return new Promise((resolve, reject) => {
+        AsyncStorage.getItem("@accesstoken").then(accesstoken => {
+            let decoded = JSON.parse(String(Buffer.from(String(accesstoken).split('.')[1], 'base64')));
+            if (parseInt(decoded.exp) >= Math.round(Date.now() / 1000.0) - 30) {
+                defaultLogin().then(res => {
+                    if (!res) {
+                        return apiRequest(endpoint, body);
+                    }
+                    else {
+                        Alert.alert('Error', `An app is required. App will close now.`, [{ text: 'Exit', onPress: () => RNExitApp.exitApp() }], { cancelable: false });
+                    }
+                }).catch(err => resolve(new ApiResponse(false, '', 'Error fetching refresh token')));
+            }
+            else {
+                fetch(`${config.server}${endpoint}`, {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: body
+                }).then((response) => response.text())
+                    .then((text) => {
+                        resolve(new ApiResponse(true, text, ''));
+                    }).catch(err => resolve(new ApiResponse(false, '', 'API error')));
+            }
+        }).catch(err => resolve(new ApiResponse(false, '', 'Storage access error')));
+    })
+}
+
+class ApiResponse {
+    success: boolean;
+    response: string;
+    error: string;
+    constructor(success: boolean, response: string, error: string) {
+        this.success = success;
+        this.response = response;
+        this.error = error;
+    }
+}
