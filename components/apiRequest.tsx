898045d7a0fe3bb8cdd79225e135f69822ab0dc6
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-08-29 18:33:02 -0400

    update wrapper

--- components/apiRequest.tsx
+++ components/apiRequest.tsx
@@ -1,6 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Alert } from 'react-native';
 import RNExitApp from 'react-native-exit-app';
+import { Buffer } from 'buffer';
 
 import config from '../config.json';
 import defaultLogin from './defaultLogin';
@@ -9,21 +10,22 @@ export default function apiRequest(endpoint: string, body: string): Promise<ApiR
     return new Promise((resolve, reject) => {
         AsyncStorage.getItem("@accesstoken").then(accesstoken => {
             let decoded = JSON.parse(String(Buffer.from(String(accesstoken).split('.')[1], 'base64')));
-            if (parseInt(decoded.exp) >= Math.round(Date.now() / 1000.0) - 30) {
+            if (parseInt(decoded.exp) <= Math.round(Date.now() / 1000.0) - 30) {
                 defaultLogin().then(res => {
                     if (!res) {
                         return apiRequest(endpoint, body);
                     }
                     else {
-                        Alert.alert('Error', `An app is required. App will close now.`, [{ text: 'Exit', onPress: () => RNExitApp.exitApp() }], { cancelable: false });
+                        Alert.alert('Error', `An app restart is required. App will close now.`, [{ text: 'Exit', onPress: () => RNExitApp.exitApp() }], { cancelable: false });
                     }
                 }).catch(err => resolve(new ApiResponse(false, '', 'Error fetching refresh token')));
             }
             else {
                 fetch(`${config.server}${endpoint}`, {
-                    method: "POST",
+                    method: "GET",
                     headers: {
-                        'Content-Type': 'application/json'
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${accesstoken}`
                     },
                     body: body
                 }).then((response) => response.text())
