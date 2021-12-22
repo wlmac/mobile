dfbc077cc20b027d2242d354161608619af56c18
mars-flat <shanec2k05@gmail.com>
2021-12-22 18:16:15 -0500

    style calendar

--- screens/CalendarScreen.tsx
+++ screens/CalendarScreen.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import { Dimensions, StyleSheet } from 'react-native';
+import React, {useState, Fragment, useCallback} from 'react';
+import { Dimensions, StyleSheet, ScrollView } from 'react-native';
 
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { Calendar, CalendarList, CalendarProps, Agenda } from 'react-native-calendars';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -11,61 +11,107 @@ const windowHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   container: {
-
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    margin: windowHeight/25,
-    fontWeight: 'bold',
-  },
+
   separator: {
-    marginVertical: 30,
+    margin: 15,
     height: 1,
     width: '80%',
   },
+
   calendar: {
-    
-    height: windowHeight * 0.55,
     width: windowWidth,
-    backgroundColor: 'transparent',
-
-    borderWidth: 5,
-    borderColor: 'black',
-    
-    
-
-    
   },
+
+  selectedDayStyle: {
+    justifyContent: 'center',
+    color: 'white',
+  }
 });
 
-const calendarParams = {
+const staticCalendarProps = {
   showSixWeeks: true,
+  disableMonthChange: true,
+  disableAllTouchEventsForDisabledDays: true,
 }
 
-// calendar theme
-const theme = {
-    
-};
+const calendarTheme = {
+  backgroundColor: '#000000',
+  calendarBackground: '#000000',
 
+  textSectionTitleColor: '#ffffff',
+  textSectionTitleDisabledColor: '#4a4a4a',
 
+  selectedDayBackgroundColor: '#ffffff',
+  selectedDayTextColor: '#ffffff',
+
+  todayTextColor: '#f7c40c',
+  dayTextColor: '#ffffff',
+
+  textDisabledColor: '#4a4a4a',
+
+  dotColor: '#00adf5',
+  selectedDotColor: '#ffffff',
+
+  arrowColor: '#ffffff',
+  disabledArrowColor: '#4a4a4a',
+
+  monthTextColor: '#ffffff',
+}
+
+// calendar screen
 export default function CalendarScreen() {
 
+  // get today's date
+  let today: string = new Date().toISOString().split('T')[0];
+
+  // selected date state
+  const [selectedDay, setSelectedDay] = useState(today);
+
+  // function that is called on day press
+  const onDayPress = useCallback((day) => {
+    setSelectedDay(day.dateString);
+    // TODO: update events on this day
+  }, []);
+
   return (
-    <View style={styles.container}>
-      {/*<Text style={styles.title}>Calendar</Text>*/}
-      <Calendar 
-  
-        style={styles.calendar}
-        theme={theme}
-        {...calendarParams} 
-        
-      />
+    // scrollview tentatively, will change potentially
+    <ScrollView contentContainerStyle={styles.container}>
+      
+      {/* --- Calendar wrapper ---*/}
+      <View style={styles.calendar}>
+
+        {/* --- Calendar component ---*/}
+        <Calendar 
+
+          onDayPress={(day) => {onDayPress(day)}}
+          theme = {calendarTheme}
+          {...staticCalendarProps}
+
+          // what to do when a day is selected
+          markedDates={{
+            [selectedDay]: {
+              selected: true,
+              disableTouchEvent: true,
+              selectedColor: '#fc1d00',
+              selectedTextColor: today == selectedDay ? '#f7c40c' : '#ffffff',
+            }
+          }}
+
+        />
+      </View>
+
+      {/* --- Separator placed maybe temporarily ---*/}
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       {/*<EditScreenInfo path="/screens/CalendarScreen.tsx" />*/}
-    </View>
+
+      {/* --- Events may be displayed here ---*/}
+      <View style={styles.container}>
+        <Text style={styles.selectedDayStyle}>{selectedDay}</Text>
+      </View>
+    </ScrollView>
   );
 }
-
-
