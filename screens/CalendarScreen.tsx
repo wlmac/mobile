e5ae48631535c0b2b6b6b2c49f8da10f7ddab89c
mars-flat <shanec2k05@gmail.com>
2021-12-21 21:43:35 -0500

    use react-native-calendars library

--- screens/CalendarScreen.tsx
+++ screens/CalendarScreen.tsx
@@ -1,27 +1,23 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
+
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
-export default function CalendarScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Calendar</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/CalendarScreen.tsx" />
-    </View>
-  );
-}
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+
     alignItems: 'center',
     justifyContent: 'center',
   },
   title: {
     fontSize: 20,
+    margin: windowHeight/25,
     fontWeight: 'bold',
   },
   separator: {
@@ -29,4 +25,47 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  calendar: {
+    
+    height: windowHeight * 0.55,
+    width: windowWidth,
+    backgroundColor: 'transparent',
+
+    borderWidth: 5,
+    borderColor: 'black',
+    
+    
+
+    
+  },
 });
+
+const calendarParams = {
+  showSixWeeks: true,
+}
+
+// calendar theme
+const theme = {
+    
+};
+
+
+export default function CalendarScreen() {
+
+  return (
+    <View style={styles.container}>
+      {/*<Text style={styles.title}>Calendar</Text>*/}
+      <Calendar 
+  
+        style={styles.calendar}
+        theme={theme}
+        {...calendarParams} 
+        
+      />
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      {/*<EditScreenInfo path="/screens/CalendarScreen.tsx" />*/}
+    </View>
+  );
+}
+
+
