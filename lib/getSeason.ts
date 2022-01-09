52739c24396becc7b60f321d506fe60d3ffae091
Patrick Lin <apocalypsecalculator@gmail.com>
2022-01-09 18:19:20 -0500

    modify OP weather function

--- lib/getSeason.ts
+++ lib/getSeason.ts
@@ -1,9 +1,9 @@
-function fromJulian (j : any) {
+function fromJulian(j: any) {
     j = (+j) + (30.0 / (24 * 60 * 60));
-    var A : any = julianArray(j, true);
+    var A: any = julianArray(j, true);
     return new Date(Date.UTC.apply(Date, A));
 };
-function julianArray (j : any, n : any) {
+function julianArray(j: any, n: any) {
     var F = Math.floor;
     var j2, JA, a, b, c, d, e, f, g, h, z;
     j += 0.5;
@@ -27,24 +27,25 @@ function julianArray (j : any, n : any) {
     if (typeof n == 'number') return JA.slice(0, n);
     return JA;
 };
-export default function getSeason (y : any, wch : any) {
-    y = y || new Date().getFullYear();
+function getSeasons() {
+    let wch: any;
+    let y: any = new Date().getFullYear();
     if (y < 1000 || y > 3000) throw y + ' is out of range';
     var Y1 = (y - 2000) / 1000,
         Y2 = Y1 * Y1,
         Y3 = Y2 * Y1,
         Y4 = Y3 * Y1;
-    var jd : any, t, w, d, est = 0,
+    var jd: any, t, w, d, est = 0,
         i = 0,
         Cos = degCos,
         A = [y],
         e1 = [485, 203, 199, 182, 156, 136, 77, 74, 70, 58, 52, 50, 45, 44, 29, 18, 17, 16, 14, 12, 12, 12, 9, 8],
         e2 = [324.96, 337.23, 342.08, 27.85, 73.14, 171.52, 222.54, 296.72, 243.58, 119.81, 297.17, 21.02,
-        247.54, 325.15, 60.93, 155.12, 288.79, 198.04, 199.76, 95.39, 287.11, 320.81, 227.73, 15.45],
+            247.54, 325.15, 60.93, 155.12, 288.79, 198.04, 199.76, 95.39, 287.11, 320.81, 227.73, 15.45],
         e3 = [1934.136, 32964.467, 20.186, 445267.112, 45036.886, 22518.443,
-        65928.934, 3034.906, 9037.513, 33718.147, 150.678, 2281.226,
-        29929.562, 31555.956, 4443.417, 67555.328, 4562.452, 62894.029,
-        31436.921, 14577.848, 31931.756, 34777.259, 1222.114, 16859.074];
+            65928.934, 3034.906, 9037.513, 33718.147, 150.678, 2281.226,
+            29929.562, 31555.956, 4443.417, 67555.328, 4562.452, 62894.029,
+            31436.921, 14577.848, 31931.756, 34777.259, 1222.114, 16859.074];
     while (i < 4) {
         switch (i) {
             case 0:
@@ -72,11 +73,31 @@ export default function getSeason (y : any, wch : any) {
     }
     return wch && A[wch] ? A[wch] : A;
 };
-function degRad (d : number) {
+function degRad(d: number) {
     return (d * Math.PI) / 180.0;
 };
-function degCos (d : number) {
+function degCos(d: number) {
     return Math.cos(degRad(d));
 };
 
 //a complete overkill method adapted to TypeScript from https://stackoverflow.com/a/5671172
+
+export default function getSeason() : string {
+    var mine = getSeasons();
+    let today = new Date();
+    let firstSpring = mine[1];
+    let firstSummer = mine[2];
+    let firstFall = mine[3];
+    let firstWinter = mine[4];
+    var season = '';
+    if (today >= firstSpring && today < firstSummer) {
+        season = 'spring';
+    } else if (today >= firstSummer && today < firstFall) {
+        season = 'summer';
+    } else if (today >= firstFall && today < firstWinter) {
+        season = 'fall';
+    } else if (today >= firstWinter || today < firstSpring) {
+        season = 'winter';
+    }
+    return season;
+}
