e3b5ffb6d8b00a08294a61d1f3c46983c7200e97
Patrick Lin <apocalypsecalculator@gmail.com>
2022-01-14 20:43:09 -0500

    add base files

---
+++ components/Changelog.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { ScrollView, StyleSheet, Alert, TouchableOpacity, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import changelog from '../changelog.json';
+
+export default function Changelog() {
+    return (
+        <View>Hello, this is the changelog!</View>
+    )
+}
