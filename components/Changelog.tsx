3a92978b002fc3db124da3d7eb2f9e8c5846cbce
Patrick Lin <apocalypsecalculator@gmail.com>
2022-01-14 22:22:52 -0500

    add changelog to settings

--- components/Changelog.tsx
+++ components/Changelog.tsx
@@ -1,11 +1,54 @@
 import * as React from 'react';
-import { ScrollView, StyleSheet, Alert, TouchableOpacity, View } from 'react-native';
+import { ScrollView, StyleSheet, Alert, TouchableOpacity, useColorScheme } from 'react-native';
+import { Text, View } from '../components/Themed';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import changelog from '../changelog.json';
 
-export default function Changelog() {
+export default function Changelog({ back }: { back: Function }) {
+    const btnBgColor = useColorScheme() === "light" ? "rgb(189, 189, 189)" : "rgb(64, 64, 64)";
+
     return (
-        <View>Hello, this is the changelog!</View>
+        <View style={styles.changelog}>
+            <Text style={styles.title}> Changelog </Text>
+            {Object.entries(changelog).map(([key, change]) => (
+                <View>
+                    <Text style={{fontSize: 20, fontWeight: 'bold'}}> v{change.version} </Text>
+                    <Text> {new Date(change.time).toLocaleString() + '\n'} </Text>
+                    <Text> {change.changes + '\n\n'} </Text>
+                </View>
+            ))}
+            <View style={{justifyContent: 'space-between', height: 100}}>
+                <TouchableOpacity style={[styles.button, { backgroundColor: btnBgColor }]} onPress={() => { back(false) }}>
+                    <Text> Back </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    changelog: {
+        flex: 1,
+        marginVertical: 15,
+        marginHorizontal: 10,
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.4,
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+    separator: {
+        marginVertical: 30,
+        height: 1,
+        width: '80%',
+    },
+    button: {
+        width: "100%",
+        borderRadius: 5,
+        alignItems: 'center',
+        padding: 10,
+        marginTop: 'auto'
+    }
+});
--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
-import { Button, StyleSheet, Alert } from 'react-native';
+import { ScrollView, StyleSheet, Alert, TouchableOpacity, useColorScheme } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { StackNavigationProp } from '@react-navigation/stack';
 
 import { Text, View } from '../components/Themed';
 import { RootStackParamList } from '../types';
+import Changelog from '../components/Changelog';
 
 export default function SettingsScreen({ navigation }: { navigation: StackNavigationProp<RootStackParamList, 'Root'> }) {
+  const [changeLogOn, setChangeLogOn] = React.useState(false);
+
+  const btnBgColor = useColorScheme() === "light" ? "rgb(189, 189, 189)" : "rgb(64, 64, 64)";
+
+  function toggleChangelog(val : boolean) {
+    setChangeLogOn(val);
+  }
   let logout = () => {
     AsyncStorage.clear().then(() => {
       Alert.alert('Success', `Logged out successfully`, [{ text: 'Ok', onPress: () => navigation.replace('Login', { loginNeeded: true }) }], { cancelable: false });
@@ -14,9 +22,16 @@ export default function SettingsScreen({ navigation }: { navigation: StackNaviga
   }
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Settings</Text>
+      <ScrollView style={changeLogOn ? {marginHorizontal: 0} : { display: "none" }}>
+        <Changelog back={toggleChangelog}></Changelog>
+      </ScrollView>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Button onPress={logout} title="Logout"></Button>
+      <TouchableOpacity style={!changeLogOn ? [styles.button, { backgroundColor: btnBgColor }] : {display: "none"}} onPress={() => {toggleChangelog(true)}}>
+        <Text> View Changelog </Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={!changeLogOn ? styles.logoutButton : {display: "none"}} onPress={logout}>
+        <Text> Logout </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -36,4 +51,19 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  logoutButton: {
+    width: "100%",
+    backgroundColor: "rgb(58, 106, 150)",
+    borderRadius: 5,
+    alignItems: 'center',
+    padding: 10,
+    marginTop: 20
+  },
+  button: {
+    width: "100%",
+    borderRadius: 5,
+    alignItems: 'center',
+    padding: 10,
+    marginBottom: 20
+  }
 });
