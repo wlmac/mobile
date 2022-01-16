daf8a58c1d10e9252b985995b82aada28b1d2592
Patrick Lin <apocalypsecalculator@gmail.com>
2022-01-16 11:33:06 -0500

    subscribe to announcements topic

--- lib/requestNotifsPerms.ts
+++ lib/requestNotifsPerms.ts
@@ -6,5 +6,16 @@ export default async function requestPerms() {
     if (enabled) {
         //handle FCM registration here
         //console.log('Authorization status:', authStatus);
+        subscribeWithRetry('announcements', 2000, 5);
     }
+}
+
+async function subscribeWithRetry(topic: string, timeout: number, retries: number) {
+    messaging().subscribeToTopic(topic).catch(err => {
+        if (retries > 0) {
+            setTimeout(() => {
+                subscribeWithRetry(topic, timeout*2, retries - 1);
+            }, timeout);
+        }
+    })
 }
