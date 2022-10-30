a24d49414b458401ff14b98a6a82537124ab1345
AAZZAZRON <aaronzhu3.1415@gmail.com>
2022-10-30 11:55:23 -0400

    announcements

---
+++ components/Announcement.tsx
@@ -0,0 +1,159 @@
+import * as React from 'react';
+import { StyleSheet, Image } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Text, View } from '../components/Themed';
+import { ThemeContext } from '../hooks/useColorScheme';
+
+var lightC = "#3a6a96";
+var darkC = "#42a4ff";
+
+export default function Announcement({ann, fullAnn}:{ann: any, fullAnn: Function}) {
+
+    const colorScheme = React.useContext(ThemeContext);
+    //console.log(ann.id, ann.title);
+    return (
+        <View style={[styles.announcement, {backgroundColor: colorScheme.scheme === 'light' ? '#f7f7f7' : '#1c1c1c', shadowColor: colorScheme.scheme === 'light' ? '#1c1c1c' : '#e6e6e6'}]}>
+            <View style={[styles.tags, {backgroundColor: colorScheme.scheme === 'light' ? '#f7f7f7' : '#1c1c1c'}]}>
+                {Object.entries(ann.tags).map(([key, tag]) => (
+                    createTag(key, tag)
+                ))}
+            </View>
+
+            {createHeader(ann.title)}
+            
+            {annDetails(ann.name, ann.icon, ann.author.slug, ann.created_date, colorScheme.scheme)}
+            {previewText(ann.body)}
+
+            {/* View More Details */}
+            <View style={[styles.click, {backgroundColor: colorScheme.scheme === 'light' ? '#f7f7f7' : '#1c1c1c'}]}>
+                <TouchableOpacity onPress={() => {fullAnn(ann.id)}}>
+                    <Text style={[{color: colorScheme.scheme === "light" ? lightC : darkC}]}>{"See announcement  >"}</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+function createTag(key: any, tag: any) {
+    return (
+        <Text key={key} style={[styles.tag, {backgroundColor: tag.color}]}>{tag.name}</Text>
+    );
+}
+
+function createHeader(title: string) {
+    return (
+        <Text style={styles.header}>{title}</Text>
+    );
+}
+
+function annDetails(org: string, orgIcon: string, author: string, timeStamp: string, colorScheme: string) {
+
+    return (
+        <View style={styles.details}>
+            <View style={[styles.detailsHeading, {backgroundColor: colorScheme === 'light' ? '#f7f7f7' : '#1c1c1c'}]}>
+                <View style={[styles.iconShadow, {shadowColor: colorScheme === "light" ? "black" : "white"}]}>
+                    <Image style={styles.orgIcon} source={{uri: orgIcon}}></Image>
+                </View>
+                <Text style={[styles.clubName, {color: colorScheme === "light" ? lightC : darkC}]}>{org}</Text>
+            </View>
+            <View style={[styles.detailsSubheading, {backgroundColor: colorScheme === 'light' ? '#f7f7f7' : '#1c1c1c'}]}>
+                <Text style={styles.timeStamp}>{new Date(timeStamp).toLocaleString("en-US", {timeZone: "EST"})}</Text>
+                <Text style={[styles.author, {color: colorScheme === "light" ? lightC : darkC}]}>{author}</Text>
+            </View>
+        </View>
+    );
+}
+
+// markdown to plaintext
+function previewText(text: string) {
+    const removeMd = require('remove-markdown');
+    const plaintext = removeMd(text);
+    return (
+        <Text style={styles.text} numberOfLines={5} >{plaintext}</Text>
+    )
+}
+
+// ----- STYLES -----
+const styles = StyleSheet.create({
+    announcement: {
+        marginVertical: 15,
+        marginHorizontal: 10,
+        paddingTop: 5,
+        paddingHorizontal:12,
+        shadowOffset: {width: 0, height: 1},
+        shadowOpacity: 0.4,
+        borderRadius: 5,
+    },
+    tags: {
+        flex: 1,
+        flexDirection: "row",
+        flexWrap: "wrap",
+        marginTop: 10,
+    },
+    tag: {
+        color: "black",
+        overflow: "hidden",
+        paddingVertical: 2,
+        paddingHorizontal: 7,
+        marginBottom: 5,
+        marginRight: 5,
+        borderRadius: 5,
+        fontSize: 13,
+    },
+    header: {
+        fontSize: 24,
+        fontWeight: "bold",
+        marginBottom: 10,
+        marginTop: 3,
+    },
+
+    details: {
+        flex:1,
+    },
+    detailsHeading:{
+        width:"100%",
+        flexDirection:"row",
+        alignItems:"center",
+        paddingBottom:5,
+    },
+    detailsSubheading:{
+        flex:1,
+    },
+    iconShadow: {
+        width: 32,
+        height: 32,
+        shadowOffset: {width: 0, height: 1},
+        shadowOpacity: 0.4,
+        shadowRadius: 2,
+        borderRadius: 32/2,
+    },
+    orgIcon: {
+        width:"100%",
+        height:"100%",
+        borderRadius: 32/2,
+    },
+    text: {
+        marginTop: 5,
+        paddingVertical: 5,
+        overflow: "hidden",
+        height: 100,
+    },
+    clubName: {
+        marginLeft:7,
+        flex:1,
+        fontWeight: "bold",
+        fontSize: 17,
+    },
+    author: {
+        marginVertical:3,
+        fontWeight: "bold",
+    },
+    timeStamp: {
+        marginVertical:3,
+        color: '#939393',
+    },
+    click: {
+        marginTop: 5,
+        marginBottom: 15,
+    },
+});
--- screens/AnnouncementScreen.tsx
+++ screens/AnnouncementScreen.tsx
@@ -1,19 +1,284 @@
-import * as React from "react";
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { StyleSheet, StatusBar, ScrollView, Image, Switch, Platform } from 'react-native';
 import { Text, View } from '../components/Themed';
+import Announcement from '../components/Announcement';
+import FullAnnouncement from '../components/FullAnnouncement';
+import * as WebBrowser from 'expo-web-browser';
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+
+import { ThemeContext } from '../hooks/useColorScheme';
+import { GuestModeContext } from '../hooks/useGuestMode';
+import apiRequest from '../lib/apiRequest';
+import config from '../config.json';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { BottomTabParamList } from '../types';
 
-import {
-  FlatList,
-  Platform,
-  StyleSheet,
-  TouchableOpacity,
-} from "react-native";
-
 export default function AnnouncementScreen({ navigation }: { navigation: BottomTabNavigationProp<BottomTabParamList, 'Announcements'> }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Announcements</Text>
-    </View>
-  );
+    // get color scheme
+    let colorScheme = React.useContext(ThemeContext);
+    const guestMode = React.useContext(GuestModeContext);
+    const loadNum = 5; // # announcements to load at a time
+
+    const emptyOrgs: {name: string, icon: string}[] = [];
+
+    // stores announcements
+    const [announcements, setAnnouncements] = useState([]);
+    const [myAnnouncements, setMyAnnouncements] = useState([]);
+    const [orgs, setOrgs] = useState(emptyOrgs);
+
+    const addOrgs = (id: number, name: string, icon: string) => {
+        let tmp = orgs;
+        tmp[id] = {name: name, icon: icon};
+        setOrgs(tmp);
+    }
+
+    // tracking how many announcements have been loaded
+    const [nextAnnSet, setNextAnnSet] = useState(0);
+    const [nextMySet, setNextMySet] = useState(0);
+
+    // loading
+    const [isLoading, toggleLoading] = useState(true); // initial loading
+    const [loadingMore, setLoadingMore] = useState(false); // loading more for lazy
+    const loadingIcon = require('../assets/images/loading.gif');
+
+    // toggle between my feed and school feed
+    const [isFilter, setFilter] = useState(false);
+    const toggleSwitch = () => setFilter(isFilter => !isFilter);
+
+    // toggle between scroll feed and full announcement feed
+    const [fullAnnId, setAnnId] = useState("-1");
+    function setFullAnnId(id: string) {
+        setAnnId(id);
+        if (id == "-1") fullA?.current?.scrollTo({x: 0, y: 0, animated: false});
+    }
+    
+    //displayed info if nothing in feed
+    const [noMyFeed, togglenoMyFeedText] = useState(false);
+
+    // scrollview reset to top on switch toggle
+    const allA = React.useRef<ScrollView>(null);
+    const myA = React.useRef<ScrollView>(null);
+    const fullA = React.useRef<ScrollView>(null);
+
+    // lazy loading check if user at bottom
+    function isCloseToBottom({layoutMeasurement, contentOffset, contentSize}: any): boolean {
+        return layoutMeasurement.height + contentOffset.y >= contentSize.height - 5;
+    }
+
+    // doesn't work properly for some reason :sob:
+    // // handle to name
+    // await apiRequest(`/api/user/${item.author.slug}?format=json`, '', 'GET').then((res) => {
+    //     if (res.success) {
+    //         let json = JSON.parse(res.response);
+    //         item.author.slug = json.first_name + " " + json.last_name;
+    //     }
+    // });
+
+
+    const onStartup = async() => {
+        // club name + club icon API requests
+        await AsyncStorage.getItem("@orgs").then((res:any) => {
+            let jsonres = JSON.parse(res);
+            if (jsonres && Array.isArray(jsonres)) {
+                for (let i = 0; i < jsonres.length; i += 1) {
+                    if (jsonres[i] != null) addOrgs(i, jsonres[i].name, jsonres[i].icon);
+                }
+            }
+        });
+
+        await loadAnnResults();
+        await loadMyResults();
+
+        if(myAnnouncements.length == 0) {
+            togglenoMyFeedText(true);
+        }
+        toggleLoading(false);
+    }
+
+    // load more "all announcements"
+    const loadAnnResults = async() => {
+        if (loadingMore) return;
+        setLoadingMore(true);
+        await apiRequest(`/api/announcements?format=json&limit=${loadNum}&offset=${nextAnnSet}`, '', 'GET', true).then((res) => {
+            if (res.success) {
+                try {
+                    let jsonres = JSON.parse(res.response).results;
+                    if (jsonres && Array.isArray(jsonres)) {
+                        jsonres.forEach((item: any) => {
+                            let orgId = item.organization.id; // gets the organization id
+                            item.icon = orgs[orgId].icon;
+                            item.name = orgs[orgId].name;
+                        });
+                        // @ts-ignore
+                        setAnnouncements(announcements.concat(jsonres));
+                        setNextAnnSet(nextAnnSet + loadNum);
+                    }
+                } catch (_e) {}
+            }
+        })
+        setLoadingMore(false);
+    }
+
+    // load more "my announcements"
+    const loadMyResults = async() => {
+        if (loadingMore) return;
+        setLoadingMore(true);
+
+        await apiRequest(`/api/announcements/feed?format=json&limit=${loadNum}&offset=${nextMySet}`, '', 'GET').then((res) => {
+            if (res.success) {
+                try {
+                    let jsonres = JSON.parse(res.response).results;
+                    if (jsonres && Array.isArray(jsonres)) {
+                        jsonres.forEach((item: any) => {
+                            let orgId = item.organization.id; // gets the organization id
+                            item.icon = orgs[orgId].icon;
+                            item.name = orgs[orgId].name;
+                        });
+                        // @ts-ignore
+                        setMyAnnouncements(myAnnouncements.concat(jsonres));
+                        setNextMySet(nextMySet + loadNum);
+                    }
+                } catch (_e) {}
+            }
+        });
+
+        setLoadingMore(false);
+    }
+    
+    // fetch data from API
+    useEffect(() => {
+        onStartup();
+    }, []);
+    
+
+    return (
+        <>
+        {/* Loading Icon */}
+        <View style={isLoading ? styles.container : {display: "none"}}>
+            <Image style={styles.loading} source={loadingIcon}/>
+        </View>
+
+        {/* After Everything is Loaded */}
+        <View style={!isLoading ? [styles.container, {backgroundColor: (colorScheme.scheme === "dark" ? "#252525" : "#eaeaea")}] : {display: "none"}}>
+            <Text style={fullAnnId == "-1" ? styles.header : {display: "none"}}>
+                {isFilter ? "My Announcements" : "All Announcements"}
+            </Text>
+
+            {/* School Announcements */}
+            <ScrollView 
+                ref={allA} 
+                style={!isFilter && fullAnnId == "-1" ? styles.scrollView : {display: "none"}}
+                onScroll={({nativeEvent}) => {
+                    if (isCloseToBottom(nativeEvent)) {
+                        loadAnnResults();
+                    }
+                }}
+                scrollEventThrottle={0}
+                >
+                    {Object.entries(announcements).map(([key, ann]) => (
+                        <Announcement key={key} ann={ann} fullAnn={setFullAnnId}></Announcement>
+                    ))}
+            </ScrollView>
+
+            {/* My Feed Announcement */}
+            <ScrollView 
+                ref={myA} 
+                style={isFilter && fullAnnId == "-1" ? styles.scrollView : {display: "none"}}
+                onScroll={({nativeEvent}) => {
+                    if (isCloseToBottom(nativeEvent)) {
+                        loadMyResults();
+                    }
+                }}
+                scrollEventThrottle={0}
+            >
+                {Object.entries(myAnnouncements).map(([key, ann]) => (
+                    <Announcement key={key} ann={ann} fullAnn={setFullAnnId}></Announcement>
+                ))}
+                <View style={[noMyFeed ? {display: "none"} : {display: "flex"}, {backgroundColor: (colorScheme.scheme === "dark" ? "#252525" : "#eaeaea")}]}>     
+                    <Text style={{textAlign: 'center'}}>There is nothing in your feed. Join some 
+                        <Text style={{color: 'rgb(51,102,187)'}} onPress={() => { WebBrowser.openBrowserAsync(config.server + '/clubs') }}>{' '}clubs{' '}</Text>
+                    to have their announcements show up here!</Text>
+                </View>
+                <View style={[guestMode.guest ? {display: "flex"} : {display: "none"}, {backgroundColor: (colorScheme.scheme === "dark" ? "#252525" : "#eaeaea")}]}>     
+                    <Text style={{textAlign: 'center'}}>
+                        <Text style={{color: 'rgb(51,102,187)'}} onPress={() => { navigation.jumpTo('Settings') }}>{' '}Log in{' '}</Text>
+                    to view your personal feed here!</Text>
+                </View>
+            </ScrollView>
+
+            {/* Full Announcement */}
+            <ScrollView ref={fullA} style={fullAnnId != "-1" ? styles.scrollView : {display: "none"}}>
+                <FullAnnouncement ann={announcements.concat(myAnnouncements).find((e: any) => e.id == fullAnnId)} backToScroll={setFullAnnId}></FullAnnouncement>
+            </ScrollView>
+
+            {/* Divider */}
+            <View
+                style={{
+                height: 3.5,
+                width: "100%",
+                backgroundColor: colorScheme.scheme === "dark" ? "#1c1c1c" : "#d4d4d4",
+                }}
+            />
+
+            {/* Filter Announcements */}
+            <View style={[fullAnnId == "-1" ? styles.row : {display: "none"}, {backgroundColor: colorScheme.scheme === 'dark' ? "#252525" : "#eaeaea",}]}>
+                <Text style={
+                    {color: isFilter ?(colorScheme.scheme === "dark" ? "#434343" : "#a8a8a8") : (colorScheme.scheme === "light" ? "#434343" : "#a8a8a8"), 
+                    fontFamily: 'poppins', 
+                    paddingHorizontal: 8, 
+                    paddingVertical: Platform.OS === 'ios' ? 5 : 10
+                }}>All </Text>
+                <Switch style={styles.switch}
+                    trackColor={{ false: "#555555", true: "#828282" }}
+                    thumbColor={isFilter ? "#444444" : "#444444"}
+                    onValueChange={() => {
+                        toggleSwitch();
+                        allA?.current?.scrollTo({x: 0, y: 0, animated: false});
+                        myA?.current?.scrollTo({x: 0, y: 0, animated: false});
+                    }}
+                    value={isFilter}
+                />
+                <Text style={
+                    {color: isFilter ?(colorScheme.scheme === "light" ? "#434343" : "#a8a8a8") : (colorScheme.scheme === "dark" ? "#434343" : "#a8a8a8"), 
+                    fontFamily: 'poppins', 
+                    paddingHorizontal:12, 
+                    paddingVertical: Platform.OS === 'ios' ? 5 : 10
+                }}>My </Text>
+            </View>
+        </View>
+        </>
+    );
 }
+
+
+
+// ----- STYLES -----
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: StatusBar.currentHeight || 0,
+    },
+    loading: {
+        width: 40,
+        height: 40,
+    },
+    header: {
+        fontSize: 20,
+        fontWeight: "bold",
+        paddingVertical: 10,
+    },
+    scrollView: {
+        marginHorizontal: 0,
+    },
+    row: {
+        marginVertical: 10,
+        flexDirection: "row", 
+    },
+    switch: {
+        paddingHorizontal:8,
+    },
+});
