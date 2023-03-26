e8b66cbcd10545ba89ebc71b47bc6dad792082b5
Creative0708 <therealcreative0708@gmail.com>
2023-03-25 22:28:53 -0400

    refactor apiRequest.ts

--- lib/apiRequest.ts
+++ lib/apiRequest.ts
@@ -4,49 +4,58 @@ import { Buffer } from 'buffer';
 import config from '../config.json';
 import defaultLogin from './defaultLogin';
 
-export default function apiRequest(endpoint: string, body: string, method: string, noAuth?: boolean): Promise<ApiResponse> {
-    return new Promise((resolve, reject) => {
-        if (noAuth) {
-            fetch(`${config.server}${endpoint}`, {
-                method: method,
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: body
-            }).then((response) => response.text())
-                .then((text) => {
-                    resolve(new ApiResponse(true, text, ''));
-                }).catch(err => resolve(new ApiResponse(false, '', 'API error')));
+export default async function apiRequest(endpoint: string, body: string, method: string, noAuth?: boolean): Promise<ApiResponse> {
+    if (noAuth) {
+        try{
+            let response = await fetch(`${config.server}${endpoint}`, {
+                method,
+                headers: { 'Content-Type': 'application/json' },
+                body
+            });
+            return new ApiResponse(true, await response.text(), '');
+        }catch(err){
+            console.error(err);
+            return new ApiResponse(false, '', 'API error');
         }
-        else {
-            AsyncStorage.getItem("@accesstoken").then((accesstoken: any) => {
-                let decoded = JSON.parse(String(Buffer.from(String(accesstoken).split('.')[1], 'base64')));
-                if (parseInt(decoded.exp) <= Math.round(Date.now() / 1000.0) - 30) {
-                    defaultLogin().then(res => {
-                        if (!res) {
-                            return apiRequest(endpoint, body, method);
-                        }
-                        else {
-                            resolve(new ApiResponse(false, '', 'An unknown error occurred'));
-                        }
-                    }).catch(err => resolve(new ApiResponse(false, '', 'Error fetching refresh token')));
+    }
+    else {
+        //TODO investigate the types of the errors and see if we can avoid nested try/catches
+        try{
+            let accesstoken = await AsyncStorage.getItem("@accesstoken");
+            let decoded = JSON.parse(String(Buffer.from(String(accesstoken).split('.')[1], 'base64')));
+            if (parseInt(decoded.exp) <= Math.round(Date.now() / 1000.0) - 30) {
+                // is this try/catch unnecessary?
+                try{
+                    if(await defaultLogin()){
+                        return new ApiResponse(false, '', 'An unknown error occurred');
+                    }else{
+                        return apiRequest(endpoint, body, method);
+                    }
+                }catch(err){
+                    console.error(err);
+                    return new ApiResponse(false, '', 'Error fetching refresh token');
                 }
-                else {
-                    fetch(`${config.server}${endpoint}`, {
-                        method: method,
+            } else {
+                try{
+                    let response = await fetch(`${config.server}${endpoint}`, {
+                        method,
                         headers: {
                             'Content-Type': 'application/json',
                             'Authorization': `Bearer ${accesstoken}`
                         },
-                        body: body
-                    }).then((response) => response.text())
-                        .then((text) => {
-                            resolve(new ApiResponse(true, text, ''));
-                        }).catch(err => resolve(new ApiResponse(false, '', 'API error')));
+                        body
+                    });
+                    return new ApiResponse(true, await response.text(), '');
+                }catch(err){
+                    console.error(err);
+                    return new ApiResponse(false, '', 'Error fetching refresh token');
                 }
-            }).catch((err: any) => resolve(new ApiResponse(false, '', 'Storage access error')));
+            }
+        }catch(err){
+            console.error(err);
+            return new ApiResponse(false, '', 'Storage access error');
         }
-    })
+    }
 }
 
 class ApiResponse {
